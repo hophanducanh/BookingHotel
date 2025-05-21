b11a9ab377e7ce35727c9fb4caeb18fbaf5d9abd
Hồ Phan Đức Anh <ducanhhophan2212@gmail.com>
2025-05-21 15:03:11 +0700

    rating activity

---
+++ app/src/main/java/com/btl/bookingHotel/adapter/CommentImageAdapter.kt
@@ -0,0 +1,30 @@
+package com.btl.bookingHotel.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.btl.bookinghotel.databinding.ItemImageBinding
+import com.bumptech.glide.Glide
+
+class CommentImageAdapter(
+    private val context: Context,
+    private val images: List<String>
+) : RecyclerView.Adapter<CommentImageAdapter.ImageViewHolder>() {
+
+    inner class ImageViewHolder(val binding: ItemImageBinding) :
+        RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ImageViewHolder {
+        val binding = ItemImageBinding.inflate(LayoutInflater.from(context), parent, false)
+        return ImageViewHolder(binding)
+    }
+
+    override fun getItemCount() = images.size
+
+    override fun onBindViewHolder(holder: ImageViewHolder, position: Int) {
+        val url =
+            "https://scaling-space-telegram-pqq5wq6pqg6c964q-5000.app.github.dev/" + images[position]
+        Glide.with(context).load(url).into(holder.binding.imageView)
+    }
+}
